ebfdc54953b23fd697a4777cf5e79331afb66446
iamdiniz <guilhermeplays03@gmail.com>
2024-09-18 14:51:53 -0300

    first commit

---
+++ Module2/add_char.py
@@ -0,0 +1,8 @@
+# as strings do Python são imutáveis. mas não limita a capacidade de operar com elas
+alphabet = "bcdefghijklmnopqrstuvwxy"
+
+alphabet = "a" + alphabet
+print(alphabet)
+alphabet = alphabet + "z"
+
+print(alphabet)
