52cc975c3746b4e82d1f66d7f201dee261a8b60b
iamdiniz <guilhermeplays03@gmail.com>
2024-12-11 20:19:48 -0300

    operador ternario e usando ele na compreensao de lista

---
+++ Module4/operador_ternario.py
@@ -0,0 +1,6 @@
+x = 7
+
+# Usando o operador ternário para decidir se x é maior ou menor que 10
+resultado = "maior que 10" if x > 10 else "menor ou igual a 10"
+
+print(resultado)
---
+++ Module4/ternario_na_compressao_de_lista.py
@@ -0,0 +1,4 @@
+# Compreensão de lista com operador ternário
+lista = ["foi par" if x % 2 == 0 else "foi impar" for x in range(10)]
+
+print(lista)
